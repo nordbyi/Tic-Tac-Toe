99389b58014433836dfea2449e44e37213773abe
Ian Nordby <ian.c.nordby@gmail.com>
2022-11-08 21:07:06 -0700

    Add query selectors, event listeners, and function to ask for user name

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-var test = document.querySelector('.test')
+var shallWe = document.querySelector('.shall-we')
+var loadInputForm = document.querySelector('#loadInputForm')
+var loadInput = document.querySelector('.loadInput')
+var greetings = document.querySelector('.greetings')
+var userName = document.querySelector('#user-name-form')
+var nameInput = document.querySelector('#nameInput')
 
+loadInputForm.addEventListener('submit', askUserName)
 
 function typewriter(text, html, i) {
-  // var p = htmlElement.innerText
-  // console.log(p)
-
-  // var pArr = p.split('')
-  console.log(i)
-  
-  // htmlElement.classList.toggle('hidden')
-  // htmlElement.innerText = ''
-
   if(i <= text.length) {
     console.log(text.substring(0, i))
     html.innerText = text.substring(0, i)
@@ -18,8 +15,16 @@ function typewriter(text, html, i) {
       typewriter(text, html, i + 1)
     }, 100)
   } else {console.log('done')}
-  
-
-  
 }
-typewriter(test.innerText, test, 0)
+
+typewriter(shallWe.innerText, shallWe, 0)
+
+function askUserName() {
+  event.preventDefault()
+  // if event
+  console.log(event)
+  userName.classList.toggle('hidden')
+  nameInput.focus()
+  typewriter(greetings.innerText, greetings, 0)
+
+}
