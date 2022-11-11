088eff767a716298a5ac54362e7e409a13a2d019
Ian Nordby <ian.c.nordby@gmail.com>
2022-11-11 13:45:12 -0700

    Add onWin method and fix checkForWin method

--- Game.js
+++ Game.js
@@ -1,25 +1,26 @@
 class Game {
   constructor(player1, player2) {
-    this.player1 = player1
-    this.player2 = player2
+    // this.player1 = player1
+    // this.player2 = player2
+    this.player1 = new Player('Turtle', 0, 'X')
+    this.player2 = new Player('Dolphin', 1, 'O')
     this.currPlayer = this.player1
-    this.board = 
-    [{index: 0, token: null}, {index: 1, token: null}, {index: 2, token: null},
-     {index: 3, token: null}, {index: 4, token: null}, {index: 5, token: null},
-     {index: 6, token: null}, {index: 7, token: null}, {index: 8, token: null}]
-
-    this.winningIndices = 
-    [
-     [0, 1, 2],
-     [3, 4, 5],
-     [6, 7, 8],
-     [0, 3, 6],
-     [1, 4, 7],
-     [2, 5, 8],
-     [0, 4, 8],
-     [2, 4, 6]
+    this.board = [
+    {index: 0, token: null}, {index: 1, token: null}, {index: 2, token: null},
+    {index: 3, token: null}, {index: 4, token: null}, {index: 5, token: null},
+    {index: 6, token: null}, {index: 7, token: null}, {index: 8, token: null}
     ]
 
+    this.winningIndices = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ]
   }
 
   changeTurn() {
@@ -27,10 +28,14 @@ class Game {
   }
 
   checkForWin() {
-    currPlayerIndices = this.board.filter(el => el.token === this.currPlayer.token).map(el => el.index)
-    for (i = 0; i < this.winningIndices.length; i++) {
+    var currPlayerIndices = this.board.filter(el => el.token === this.currPlayer.token).map(el => el.index)
+    console.log(currPlayerIndices)
+    for (var i = 0; i < this.winningIndices.length; i++) {
       if(this.winningIndices[i].every(el => currPlayerIndices.includes(el))) {
-        // game win
+        this.onWin()
+        console.log(`${this.currPlayer.name} Wins!`)
+        // game win update data model function?
+        // return true to break out of checking for tie if checking for tie in same place?
       }
     }
   }
@@ -39,6 +44,10 @@ class Game {
     if(this.board.every(el => el.token !== null)) {
       return true
     }
+  }
+
+  onWin() {
+    this.currPlayer.increaseWins()
   }
 
   reset() {
