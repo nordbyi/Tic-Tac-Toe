45b4f75d07dee2cbf742720195a4d408c220c6b6
Ian Nordby <ian.c.nordby@gmail.com>
2022-11-11 10:33:46 -0700

    Add checkForWin function and chage this.board to array of objects

--- Game.js
+++ Game.js
@@ -2,10 +2,11 @@ class Game {
   constructor(player1, player2) {
     this.player1 = player1
     this.player2 = player2
+    this.currPlayer = this.player1
     this.board = 
-    [0, 1, 2,
-     3, 4, 5,
-     6, 7, 8]
+    [{index: 0, token: null}, {index: 1, token: null}, {index: 2, token: null},
+     {index: 3, token: null}, {index: 4, token: null}, {index: 5, token: null},
+     {index: 6, token: null}, {index: 7, token: null}, {index: 8, token: null}]
 
     this.winningIndices = 
     [
@@ -19,14 +20,20 @@ class Game {
      [2, 4, 6]
     ]
 
-    this.turn = true
   }
 
   changeTurn() {
-    this.turn = !this.turn
+    this.currPlayer = this.currPlayer === this.player1 ? this.player2 : this.player1
   }
 
-  checkForWin() {}
+  checkForWin() {
+    currPlayerIndices = this.board.filter(el => el.token === this.currPlayer.token).map(el => el.index)
+    for (i = 0; i < this.winningIndices.length; i++) {
+      if(this.winningIndices[i].every(el => currPlayerIndices.includes(el))) {
+        // game win
+      }
+    }
+  }
 
   checkForTie() {}
 
